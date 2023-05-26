17bb87801f70a0cfbb4b830f0843704330e698dc
hagre09 <76846967+hagre09@users.noreply.github.com>
2023-05-26 18:37:31 +0300

    Footer Comp

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css"
+
+function Footer() {
+    return <>
+       <footer>
+            &copy; CopyWright Hagr Badran 2023
+      </footer>
+    
+    </>
+}
+
+export default Footer;
